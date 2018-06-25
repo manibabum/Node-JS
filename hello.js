aaf73882901c388bffddceb0dd436611512310f8
mani <manibabu.muddapu01@gmail.com>
2018-06-25 09:41:45 +0530

    Hello.js

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Mani!\n'); // build should pass now!
-}).listen(1111, 'localhost');
-console.log('Server running at http://localhost:1111/');
+}).listen(1111, 'manibabu.azurewebsites.net');
+console.log('Server running at https://manibabu.azurewebsites.net:1111/');
